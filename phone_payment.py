f1a3910f193786e8943bd245ac31ad1125e01455
vitorrafael <vrsilveira07@gmail.com>
2019-11-20 15:14:16 -0300

    Inserts the script into a infinite loop with an option to break the script

--- phone_payment.py
+++ phone_payment.py
@@ -8,18 +8,20 @@ def main():
 
     spreadsheet = Spreadsheet(creds)
 
-    operation = int(input('Operations:\n1 - Read sheet\n2 - Update sheet\n'))
-    if operation == 1:  
-        values = spreadsheet.get_spreadsheet_values("A1:E12")
-        for row in values:
-            print(row)
-    elif operation == 2:
-        payment_one = int(input('Payment One: '))
-        payment_two = int(input('Payment Two: '))
-        month = int(input('Month: '))
-        spreadsheet.insert_values_in_sheet(payment_one, payment_two, month)
-    else:
-        print('Invalid operation!')
+    while True:
+        operation = int(input('Operations:\n1 - Read sheet\n2 - Update sheet\n3 - Cancel'))
+        if operation == 1:  
+            values = spreadsheet.get_spreadsheet_values("A1:E12")
+            spreadsheet.print_sheet(values)
+        elif operation == 2:
+            payment_one = int(input('Payment One: '))
+            payment_two = int(input('Payment Two: '))
+            month = int(input('Month: '))
+            spreadsheet.insert_values_in_sheet(payment_one, payment_two, month)
+        elif operation == 3:
+            break
+        else:
+            print('Invalid operation!')
 
 if __name__ == '__main__':
     main()
